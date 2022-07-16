b4b1e9ac2d0dd28006130aeb485b269ec18d227c
user <valikofficial4@gmail.com>
2022-07-16 15:30:52 +0300

    Nexum App 1.0 | 16.07.2022

---
+++ app/src/main/java/com/example/nexum/MainActivity.kt
@@ -0,0 +1,57 @@
+package com.example.nexum
+
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import android.widget.Button
+import android.widget.EditText
+import android.widget.Toast
+
+
+class MainActivity : AppCompatActivity() {
+
+    private var first_field: EditText? = null
+    private var second_field: EditText? = null
+    private var button: Button? = null
+    private var btn_clear: Button? = null
+
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        //Находим элементы
+        first_field = findViewById(R.id.first_field)
+        second_field = findViewById(R.id.second_field)
+        button = findViewById(R.id.button)
+        btn_clear = findViewById(R.id.btn_clear)
+
+        //Обработчик кнопок
+        //button.setOnClickListener(this);
+        //btn_clear.setOnClickListener(this);
+
+        // читаем EditText и заполняем переменные числами
+        //num1 = Float.parseFloat(first_field.getText().toString());
+        //num2 = Float.parseFloat(second_field.getText().toString());
+
+        // читаем EditText и заполняем переменные числами
+        //num1 = Float.parseFloat(first_field.getText().toString());
+        //num2 = Float.parseFloat(second_field.getText().toString());
+
+        // определяем нажатую кнопку и выполняем соответствующую операцию
+        // в oper пишем операцию, потом будем использовать в выводе
+       // switch (v.getId()) {
+            //case R.id.button:
+            //oper = "+";
+            //result = num1 + num2;
+            //break;
+
+
+        button?.setOnClickListener {
+            if(first_field?.text?.toString()?.trim()?.equals("")!!){
+                Toast.makeText(this, "Введите 1 число", Toast.LENGTH_LONG).show()
+            }
+        }
+
+    }
+}
